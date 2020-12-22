1ebc8662614a7b335cabeb615cf26e343a337033
Suvankar Das <iambabai0@gmail.com>
2020-12-22 13:10:19 +0530

    props in react

--- src/index.js
+++ src/index.js
@@ -4,23 +4,40 @@ import ReactDOM from 'react-dom';
 
 import '../src/style.css';
 
+const firstBookInformation = {
+	bookName: 'A Promised Land',
+	authorName: 'Barack Obama',
+	bookCover: 'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg'
+};
+const secondBookInformation = {
+	bookName: 'Interesting Stories For Curious People',
+	authorName: "Bill O'Neill",
+	bookCover: 'https://images-na.ssl-images-amazon.com/images/I/71EEdvsvd1L._AC_UL200_SR200,200_.jpg'
+};
+
 const BookList = () => {
 	return (
 		<section className="booklist">
-			<Book />
+			<Book
+				bookName={firstBookInformation.bookName}
+				authorName={firstBookInformation.authorName}
+				bookCover={firstBookInformation.bookCover}
+			/>
+			<Book
+				bookName={secondBookInformation.bookName}
+				authorName={secondBookInformation.authorName}
+				bookCover={secondBookInformation.bookCover}
+			/>
 		</section>
 	);
 };
 
-const authorName = 'Barack Obama';
-
-const Book = () => {
-	const bookName = 'A Promised Land';
+const Book = (props) => {
 	return (
 		<article className="book">
-			<img src="https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg" />
-			<h1>{bookName}</h1>
-			<h4>{authorName.toUpperCase()}</h4>
+			<img src={props.bookCover} />
+			<h1>{props.bookName}</h1>
+			<h4>-{props.authorName}</h4>
 		</article>
 	);
 };
