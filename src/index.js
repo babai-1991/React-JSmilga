257932826f96d39a2e780b667876907acdad7bb6
Suvankar Das <iambabai0@gmail.com>
2020-12-22 19:24:22 +0530

    key props and spread operator

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import '../src/style.css';
 
 const booksArray = [
 	{
+		id: '1',
 		bookName: 'A Promised Land',
 		authorName: 'Barack Obama',
 		bookCover: 'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg'
 	},
 	{
+		id: '2',
 		bookName: 'Interesting Stories For Curious People',
 		authorName: "Bill O'Neill",
 		bookCover: 'https://images-na.ssl-images-amazon.com/images/I/71EEdvsvd1L._AC_UL200_SR200,200_.jpg'
@@ -21,15 +23,14 @@ const BookList = () => {
 	return (
 		<section className="booklist">
 			{booksArray.map((book) => {
-				return <Book bookInfo={book} />;
+				return <Book key={book.id} {...book} />;
 			})}
 		</section>
 	);
 };
 
 const Book = (props) => {
-	const { bookCover, bookName, authorName } = props.bookInfo;
-
+	const { bookCover, bookName, authorName } = props;
 	return (
 		<article className="book">
 			<img src={bookCover} />
