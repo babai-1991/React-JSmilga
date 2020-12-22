1cbe3e3ba4b0780072226df47673a457656e65f1
Suvankar Das <iambabai0@gmail.com>
2020-12-22 15:12:29 +0530

    Proper list rendering

--- src/index.js
+++ src/index.js
@@ -4,15 +4,39 @@ import ReactDOM from 'react-dom';
 
 import '../src/style.css';
 
-const todoArray = [ 'Wake up at 9 A.M', 'BreakFast', 'Study' ];
+const booksArray = [
+	{
+		bookName: 'A Promised Land',
+		authorName: 'Barack Obama',
+		bookCover: 'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg'
+	},
+	{
+		bookName: 'Interesting Stories For Curious People',
+		authorName: "Bill O'Neill",
+		bookCover: 'https://images-na.ssl-images-amazon.com/images/I/71EEdvsvd1L._AC_UL200_SR200,200_.jpg'
+	}
+];
 
-const todoItem = todoArray.map((item, i) => {
-	return <h1>{item}</h1>;
-});
-
-const Todo = () => {
-	console.log(todoItem);
-	return <div>{todoItem}</div>;
+const BookList = () => {
+	return (
+		<section className="booklist">
+			{booksArray.map((book) => {
+				return <Book bookInfo={book} />;
+			})}
+		</section>
+	);
 };
 
-ReactDOM.render(<Todo />, document.getElementById('root'));
+const Book = (props) => {
+	const { bookCover, bookName, authorName } = props.bookInfo;
+
+	return (
+		<article className="book">
+			<img src={bookCover} />
+			<h1>{bookName}</h1>
+			<h4>-{authorName}</h4>
+		</article>
+	);
+};
+
+ReactDOM.render(<BookList />, document.getElementById('root'));
