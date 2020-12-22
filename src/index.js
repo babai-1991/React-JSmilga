d64c5a069ab057915e320ef6bcc3e35d38f67efc
Suvankar Das <iambabai0@gmail.com>
2020-12-22 20:42:33 +0530

    event basic react

--- src/index.js
+++ src/index.js
@@ -19,6 +19,20 @@ const booksArray = [
 	}
 ];
 
+/*=============== 
+Custom Handler
+=================
+*/
+
+const infoHandler = () => {
+	alert('What to want to know');
+};
+
+const moreInfoHandler = (event, author, bookTitle) => {
+	console.log(event);
+	alert(`Thanks for choosing ${author}'s ${bookTitle}`);
+};
+
 const BookList = () => {
 	return (
 		<section className="booklist">
@@ -36,6 +50,12 @@ const Book = (props) => {
 			<img src={bookCover} />
 			<h1>{bookName}</h1>
 			<h4>-{authorName}</h4>
+			<button type="button" className="btn" onClick={() => infoHandler()}>
+				Read More
+			</button>
+			<button type="button" className="btn" onClick={(e) => moreInfoHandler(e, authorName, bookName)}>
+				Proceed To Buy
+			</button>
 		</article>
 	);
 };
