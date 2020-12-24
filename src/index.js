2f1cd33611390e325556e205d94531410fc2e46f
Suvankar Das <iambabai0@gmail.com>
2020-12-24 12:00:10 +0530

    react advanced tut setup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-//index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Book from './Book';
-import '../src/style.css';
+import './index.css';
+import App from './App';
 
-import { books } from './books';
-
-const BookList = () => {
-	return (
-		<section className="booklist">
-			{books.map((book) => {
-				return <Book key={book.id} {...book} />;
-			})}
-		</section>
-	);
-};
-
-ReactDOM.render(<BookList />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
