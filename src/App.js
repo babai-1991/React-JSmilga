405157d5fec5677cefad8f39c159f5058c919ecb
Suvankar Das <iambabai0@gmail.com>
2020-12-24 12:44:39 +0530

    1-useState - Simple Use Case

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+//App.js
+import React from 'react';
+import Setup from './tutorial/1-useState/setup/1-error-example';
+
 function App() {
-  return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
-    </div>
-  )
+	return (
+		<div className="container">
+			<Setup />
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,21 @@
+//1-error-example.js
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+	let title = 'Hello Guest';
+
+	const getMemberShip = () => {
+		title = 'Thanks for the membership';
+		console.log(title);
+	};
+	return (
+		<React.Fragment>
+			<h2>{title}</h2>
+			<button type="button" className="btn" onClick={getMemberShip}>
+				Subscribe
+			</button>
+		</React.Fragment>
+	);
 };
 
 export default ErrorExample;
