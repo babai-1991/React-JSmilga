62c7fd380588237198423318054a6de0a2af5d80
Suvankar Das <iambabai0@gmail.com>
2020-12-25 20:57:43 +0530

    usestate functional form

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,10 +1,10 @@
+//5-useState-counter.js
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
 	let [ value, setValue ] = useState(0);
 
 	const calculateValue = (param) => {
-		debugger;
 		switch (param) {
 			case 'increase':
 				setValue(++value);
@@ -16,6 +16,16 @@ const UseStateCounter = () => {
 				setValue(0);
 				break;
 		}
+	};
+	const complexCalculate = () => {
+		setTimeout(() => {
+			//setValue(value + 1);
+			//always must return or functionality will not work
+			//prevState will hold the updated value instead of the old value.
+			setValue((prevState) => {
+				return prevState + 1;
+			});
+		}, 2000);
 	};
 	return (
 		<React.Fragment>
@@ -29,6 +39,14 @@ const UseStateCounter = () => {
 					Reset
 				</button>
 				<button className="btn" onClick={() => calculateValue('increase')}>
+					Increase
+				</button>
+			</section>
+			<section style={{ margin: '4rem 0' }}>
+				<h2>Complex Counter</h2>
+				<h1>{value}</h1>
+
+				<button className="btn" onClick={complexCalculate}>
 					Increase
 				</button>
 			</section>
