72b91451b060fa9d3d4870389cb53567be6d10c7
Suvankar Das <iambabai0@gmail.com>
2020-12-25 12:02:40 +0530

    useState array example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //App.js
 import React from 'react';
-import Setup from './tutorial/1-useState/setup/2-useState-basics';
+import Setup from './tutorial/1-useState/setup/3-useState-array';
 
 function App() {
 	return (
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,49 @@
-import React from 'react';
+//3-useState-array.js
+import React, { useState } from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+	const [ person, setPerson ] = useState(data);
+
+	/**************
+   Event handlers
+  ****************
+   */
+	const clearArray = () => {
+		setPerson([]); // passing empty array
+	};
+
+	const removeItem = (id) => {
+		const newArray = person.filter((people) => {
+			return people.id !== id; // return those items whose id is not matching
+		});
+
+		setPerson(newArray);
+	};
+
+	/*********************
+  End Of Event handlers
+************************
+*/
+
+	return (
+		<React.Fragment>
+			{person.map((people) => {
+				const { id, name } = people;
+				return (
+					<div key={id} className="item">
+						{name}
+						<button type="button" onClick={() => removeItem(id)}>
+							Remove
+						</button>
+					</div>
+				);
+			})}
+			<button type="button" className="btn" onClick={clearArray}>
+				Clear All
+			</button>
+		</React.Fragment>
+	);
 };
 
 export default UseStateArray;
