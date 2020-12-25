5a4536dec90f8a8f2063211e0b835c3c11d19a60
Suvankar Das <iambabai0@gmail.com>
2020-12-25 10:48:16 +0530

    useState basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
+//2-useState-basics.js
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+	const [ text, setText ] = useState('Hello Guest');
+	const getMembership = () => {
+		if (text == 'Hello Guest') {
+			setText('Thanks for subscribing');
+		} else {
+			setText('Hello Guest');
+		}
+	};
+	return (
+		<React.Fragment>
+			<h2>{text}</h2>
+			<button type="button" className="btn" onClick={getMembership}>
+				Subscribe
+			</button>
+		</React.Fragment>
+	);
 };
 
 export default UseStateBasics;
