0cc6267ffd9ae3e3b4acffcd0d2f5d0aca42caea
Suvankar Das <iambabai0@gmail.com>
2020-12-25 12:44:09 +0530

    useState object example

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,42 @@
+//4-useState-object.js
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+	const personObj = {
+		Name: 'Peter',
+		Age: 29,
+		Message: 'Hello Peter'
+	};
+
+	const [ person, setPerson ] = useState(personObj);
+
+	/*
+********************
+Event handler
+********************
+*/
+	const changeMessageProperty = () => {
+		setPerson({
+			...personObj, //spreading/copying whole obj first and then only override message
+			Message: 'Changed Message'
+		});
+	};
+
+	/*
+********************
+End of Event handler
+********************
+*/
+	return (
+		<React.Fragment>
+			<h3>{person.Name}</h3>
+			<h3>{person.Age}</h3>
+			<h3>{person.Message}</h3>
+			<button type="button" className="btn" onClick={changeMessageProperty}>
+				Change Message
+			</button>
+		</React.Fragment>
+	);
 };
 
 export default UseStateObject;
