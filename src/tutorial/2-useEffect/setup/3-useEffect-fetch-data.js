b9b3639739bef0c27f162efb9e920a23a3adaa5f
Suvankar Das <iambabai0@gmail.com>
2020-12-26 20:03:49 +0530

    useEffect fetch external data

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,43 @@
+//3-useEffect-fetch-data.js
 import React, { useState, useEffect } from 'react';
 
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+	const [ users, setUsers ] = useState([]);
+
+	const getUsers = async () => {
+		const response = await fetch(url);
+		const users = await response.json();
+		//console.log(users);
+		setUsers(users);
+	};
+
+	useEffect(() => {
+		getUsers();
+	}, []);
+
+	return (
+		<React.Fragment>
+			<h3>github users</h3>
+			<ul className="users">
+				{users.map((user) => {
+					const { id, login, avatar_url, html_url } = user;
+					return (
+						<li key={id}>
+							<img src={avatar_url} alt={login} />
+							<div>
+								<h4>{login}</h4>
+								<a href={html_url} target="_blank">
+									profile
+								</a>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
+		</React.Fragment>
+	);
 };
 
 export default UseEffectFetchData;
