8fca7c6a6d881a25a8ce0c4858a0e05fe7ba37df
Suvankar Das <iambabai0@gmail.com>
2020-12-27 12:44:52 +0530

    multiple return fetching error loading example

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,56 @@
+//1-multiple-returns.js
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
+//error in  url for testing error,uncomment this and comment previous line
+//const url = 'https://api.github.com/users/QuincyLarson1';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+	const [ loading, setLoading ] = useState(true);
+	const [ isError, setIsError ] = useState(false);
+	const [ user, setUser ] = useState([]);
+
+	useEffect(() => {
+		setTimeout(() => {
+			fetch(url)
+				.then((resp) => {
+					if (resp.status >= 200 && resp.status <= 299) {
+						//must must must user return keyword
+						return resp.json();
+					} else {
+						setLoading(false);
+						setIsError(true);
+					}
+				})
+				.then((user) => {
+					const { login } = user;
+					//as we get user back , we can then hide loading.
+					setLoading(false);
+					setUser(login);
+				})
+				.catch(function(error) {
+					console.log(error);
+				});
+		}, 2000);
+	}, []);
+
+	if (loading) {
+		return (
+			<div>
+				<h2>Loading...</h2>
+			</div>
+		);
+	}
+	if (isError) {
+		return (
+			<div>
+				<h2>There was an error...</h2>
+			</div>
+		);
+	}
+	return (
+		<div>
+			<h2>{user}</h2>
+		</div>
+	);
 };
 
 export default MultipleReturns;
